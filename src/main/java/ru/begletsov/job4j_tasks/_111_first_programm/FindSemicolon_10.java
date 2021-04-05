73d59ce1151c4aaac96e685540d0fcec5c9405de
Sergei Begletsov <snower87@yandex.ru>
2021-04-05 17:41:56 +0500

    30. Решена задача 1. Забытая точка с запятой из раздела 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/FindSemicolon_10.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 1. Забытая точка с запятой
+ * @Описание Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+ * @author Sergei Begletsov
+ * @since 05.04.2021
+ * @version 1
+ */
+
+public class FindSemicolon_10 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
