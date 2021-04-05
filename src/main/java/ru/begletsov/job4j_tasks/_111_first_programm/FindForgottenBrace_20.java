2ae36965c876aef0237789f889b1d208ff986391
Sergei Begletsov <snower87@yandex.ru>
2021-04-05 18:28:04 +0500

    31. Решена задача 2. Забытая скобка из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/FindForgottenBrace_20.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 2. Забытая скобка
+ * @Описание Весь код в Java пишется внутри блоков ограниченных фигурными скобками { и }. Исправьте ошибку в приведенном коде.
+ * @author Sergei Begletsov
+ * @since 05.04.2021
+ * @version 1
+ */
+
+public class FindForgottenBrace_20 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
