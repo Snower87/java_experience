fed60b9152bf7b4bea316a78467ba1a56202abdf
Sergei Begletsov <snower87@yandex.ru>
2021-04-06 12:36:51 +0500

    35. Решена задача "2.2. Не закрытый класс" из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/NotClosedClass_22.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 2.2. Не закрытый класс
+ * @Описание Ниже приведен код в котором забыли закрыть блок класса. Поправьте ошибку
+ * @author Sergei Begletsov
+ * @since 06.04.2021
+ * @version 1
+ */
+
+public class NotClosedClass_22 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/test/java/ru/begletsov/job4j_tasks/_111_first_programm/NotClosedClass_22Test.java
@@ -0,0 +1,17 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class NotClosedClass_22Test {
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        NotClosedClass_22.main(null);
+        assertThat(out.toString(), is("Hello, Job4j!" + System.lineSeparator()));
+    }
+}
