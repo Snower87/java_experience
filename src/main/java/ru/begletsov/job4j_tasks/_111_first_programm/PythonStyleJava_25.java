69e729d11fdd8c076e15915944f16c71084cd0a7
Sergei Begletsov <snower87@yandex.ru>
2021-04-06 11:07:20 +0500

    32. Решена задача "2.5. Python стиль в Java" из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/PythonStyleJava_25.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 2.5. Python стиль в Java
+ * @Описание Программист из Python решил написать код на Java. В Python блоки разделяются отступами, а в Java фигурными скобками. Поправьте код
+ * @author Sergei Begletsov
+ * @since 06.04.2021
+ * @version 1
+ */
+
+public class PythonStyleJava_25 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
