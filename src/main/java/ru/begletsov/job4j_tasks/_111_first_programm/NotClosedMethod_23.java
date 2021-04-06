d6326c3827faef40124c0f9c545e1babe57776ef
Sergei Begletsov <snower87@yandex.ru>
2021-04-06 12:46:34 +0500

    36. Решена задача "2.3. Не закрытый метод" из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/NotClosedMethod_23.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 2.3. Не закрытый метод
+ * @Описание В методе main забыли закрывающую скобку. Поправь код приведенный ниже.
+ * @author Sergei Begletsov
+ * @since 06.04.2021
+ * @version 1
+ */
+
+public class NotClosedMethod_23 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
