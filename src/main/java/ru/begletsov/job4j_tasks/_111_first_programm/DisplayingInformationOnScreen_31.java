0fbf24326dcd6993b2b054e65ec4a32922480fb6
Sergei Begletsov <snower87@yandex.ru>
2021-04-06 13:05:08 +0500

    37. Решена задача "3.1. Вывод информации на экран" из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/DisplayingInformationOnScreen_31.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 3.1. Вывод информации на экран
+ * @Описание Допишите программу приведенную ниже. Программа должна выводить на консоль строку "Hello, Job4j!"
+ * @author Sergei Begletsov
+ * @since 06.04.2021
+ * @version 1
+ */
+
+public class DisplayingInformationOnScreen_31 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
