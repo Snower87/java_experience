f8f122582e06c4d3a5245d15ff2c9cfc500dc784
Sergei Begletsov <snower87@yandex.ru>
2021-04-06 12:17:40 +0500

    33. Решена задача "2.4. Скобки только фигурные" из раздела: 1.1.1. Первая программа

---
+++ src/main/java/ru/begletsov/job4j_tasks/_111_first_programm/OnlyBrace_24.java
@@ -0,0 +1,15 @@
+package ru.begletsov.job4j_tasks._111_first_programm;
+
+/**
+ * @Задание 2.4. Скобки только фигурные
+ * @Описание Начинающий Java программист решил заменить фигурные скобки на круглые. Так делать нельзя. Java требует использовать только фигурные скобки для блоков. Поправьте код
+ * @author Sergei Begletsov
+ * @since 06.04.2021
+ * @version 1
+ */
+
+public class OnlyBrace_24 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
