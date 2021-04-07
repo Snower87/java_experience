16b0b0c2445540c24c2642ab39676c5d2646456e
Sergei Begletsov <snower87@yandex.ru>
2021-04-07 16:30:26 +0500

    48. Решена задача "8. Модуль числа" из раздела: 1.1.2. Тип данных

---
+++ src/main/java/ru/begletsov/job4j_tasks/_112_data_types/MathAbs_8.java
@@ -0,0 +1,20 @@
+package ru.begletsov.job4j_tasks._112_data_types;
+
+/**
+ * @Задание 8. Модуль числа
+ * @Описание В Java модуль можно получить с использованием функции Math.abs. Синтаксис такой
+ * double переменная = Math.abs(число);
+ * Пример:
+ *      double result = Math.abs(-3); // = 3
+ * Ниже приведен код. Вам нужно его раскоментировать и получить модуль -5
+ * @author Sergei Begletsov
+ * @since 07.04.2021
+ * @version 1
+ */
+
+public class MathAbs_8 {
+    public static void main(String[] args) {
+        double result = Math.abs(-5);
+        System.out.println(result);
+    }
+}
