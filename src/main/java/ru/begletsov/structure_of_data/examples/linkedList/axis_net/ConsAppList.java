940a72345b7e9a11c63fe8183bd8ee3c83ba6a99
Sergei Begletsov <snower87@yandex.ru>
2021-04-23 17:05:54 +0500

    115. Создание и разбор алгоритма работы односвязанного списка (http://axis.bplaced.net/news/773)

---
+++ src/main/java/ru/begletsov/structure_of_data/examples/linkedList/axis_net/ConsAppList.java
@@ -0,0 +1,28 @@
+package ru.begletsov.structure_of_data.examples.linkedList.axis_net;
+
+public class ConsAppList {
+    public static void main(String[] args) {
+        System.out.println("Example view action with List:");
+        List ml = new List();
+        ml.addBack(1);
+        ml.addBack(12);
+        ml.addBack(3);
+        ml.addBack(9);
+        ml.addBack(0);
+
+        System.out.println("Printing all elements List after add 5 elements:");
+        ml.printList();
+        System.out.println();
+
+        ml.deleteElement(6);
+        ml.deleteElement(5);
+        ml.deleteElement(12);
+        ml.deleteElement(0);
+        ml.deleteElement(4);
+        ml.deleteElement(1);
+
+        System.out.println("Printing all elements List after delete some elements:");
+        ml.printList();
+        System.out.println();
+    }
+}
---
+++ src/main/java/ru/begletsov/structure_of_data/examples/linkedList/axis_net/ListElement.java
@@ -0,0 +1,20 @@
+package ru.begletsov.structure_of_data.examples.linkedList.axis_net;
+
+/**
+ * @Задание Создание и разбор алгоритма работы односвязанного списка
+ * @Описание Структуры данных. Список
+ *           ссылка на статью: http://axis.bplaced.net/news/773
+ * @author Sergei Begletsov
+ * @since 23.04.2021
+ * @version 1
+ */
+
+public class ListElement {
+    /**
+     * Каждый элемент списка содержит в себе данные и указатель на следующий элемент.
+     * Возьмем простой вариант данных в списке - просто целые числа.
+     */
+
+    ListElement next;    // указатель на следующий элемент
+    int data;            // данные
+}
