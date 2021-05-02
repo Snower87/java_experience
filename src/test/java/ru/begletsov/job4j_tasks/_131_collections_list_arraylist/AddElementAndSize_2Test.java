409758ee51a699a5bb59f212cce797ce6bea389e
Sergei Begletsov <snower87@yandex.ru>
2021-05-02 17:05:15 +0600

    138. Решена задача "2. Размер списка и добавление элементов. Методы: size(), add(E e)" из раздела: 1.3.1. Коллекция List, ArrayList

---
+++ src/test/java/ru/begletsov/job4j_tasks/_131_collections_list_arraylist/AddElementAndSize_2Test.java
@@ -0,0 +1,21 @@
+package ru.begletsov.job4j_tasks._131_collections_list_arraylist;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AddElementAndSize_2Test {
+    @Test
+    public void addNewElement() {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        boolean result = AddElementAndSize_2.addNewElement(list, "fourth");
+        assertThat(result, is(true));
+    }
+}
