7fe6f66add1d283a3bdbc1b18d995e874116122f
Sergei Begletsov <snower87@yandex.ru>
2021-05-04 11:13:21 +0500

    143. Решена задача "7. Добавление списка в другой список. Методы: boolean addAll​(Collection<? extends E> c), boolean remove(E e)" из раздела: 1.3.1. Коллекция List, ArrayList

---
+++ src/test/java/ru/begletsov/job4j_tasks/_131_collections_list_arraylist/AddAllElementsList_7Test.java
@@ -0,0 +1,55 @@
+package ru.begletsov.job4j_tasks._131_collections_list_arraylist;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AddAllElementsList_7Test {
+    @Test
+    public void containsElement() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("second");
+        right.add("fifth");
+        String check = "second";
+        int result = AddAllElementsList_7.containsElement(left, right, check);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void containsElementInFirstList() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("fifth");
+        String check = "second";
+        int result = AddAllElementsList_7.containsElement(left, right, check);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void notContainsElement() {
+        List<String> left = new ArrayList<>();
+        left.add("first");
+        left.add("second");
+        left.add("third");
+        List<String> right = new ArrayList<>();
+        right.add("fourth");
+        right.add("fifth");
+        right.add("sixth");
+        String check = "bird";
+        int result = AddAllElementsList_7.containsElement(left, right, check);
+        assertThat(result, is(-1));
+    }
+}
