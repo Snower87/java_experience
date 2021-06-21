48518ebe1b18932139a02abe791b2ecf36e10d21
Sergei Begletsov <snower87@yandex.ru>
2021-06-21 15:53:00 +0500

    201. Решена задача "1. Добавление элементов в список ArrayList" из раздела: Коллекция ArrayList

---
+++ src/main/java/ru/begletsov/kodesource_tasks/_1_collections_list_arraylist/Exercise_1.java
@@ -0,0 +1,29 @@
+package ru.begletsov.kodesource_tasks._1_collections_list_arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Задание 1. Добавление элементов в список ArrayList
+ * @Раздел Коллекция ArrayList
+ * @Описание Напишите программу на Java, чтобы создать новый список массивов, добавить несколько цветов (строку)
+ * и распечатать коллекцию.
+ * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
+ * @author Sergei Begletsov
+ * @since 21.06.2021
+ * @version 1
+ */
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("red");
+        list.add("orange");
+        list.add("white");
+        list.add("red");
+        list.add("red");
+        list.add("blue");
+        list.add("black");
+        System.out.println(list);
+    }
+}
