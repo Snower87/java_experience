fe6a7d9f153c158ca90e0f5d7c5343f2973cef19
Sergei Begletsov <snower87@yandex.ru>
2021-06-24 11:43:43 +0500

    211. Решена задача "11. Вывести индексы и элементы списка" из раздела: Коллекция LinkedList

---
+++ src/main/java/ru/begletsov/kodesource_tasks/_1_collections_list_arraylist/_11_IndexAndElements.java
@@ -0,0 +1,10 @@
+package ru.begletsov.kodesource_tasks._1_collections_list_arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class _11_IndexAndElements {
+
+}
---
+++ src/main/java/ru/begletsov/kodesource_tasks/_2_collections_list_linkedlist/_11_IndexAndElements.java
@@ -0,0 +1,28 @@
+package ru.begletsov.kodesource_tasks._2_collections_list_linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Задание 11. Вывести индексы и элементы списка
+ * @Раздел Коллекция LinkedList
+ * @Описание Напишите Java-программу для отображения элементов и их позиций в связанном списке.
+ * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
+ * @author Sergei Begletsov
+ * @since 24.06.2021
+ * @version 1
+ */
+
+public class _11_IndexAndElements {
+    public static void main(String[] args) {
+        //create list and add some elements
+        List<String> colors = new ArrayList<String>();
+        colors.add("red");
+        colors.add("white");
+        colors.add("blue");
+        colors.add("green");
+        for (int index = 0; index < colors.size(); index++) {
+            System.out.println("Index " + index + ", element: " + colors.get(index));
+        }
+    }
+}
