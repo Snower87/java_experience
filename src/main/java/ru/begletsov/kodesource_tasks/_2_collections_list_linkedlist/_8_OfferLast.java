33128551a8ced53bba2507fd67424a8ab92f4ce8
Sergei Begletsov <snower87@yandex.ru>
2021-06-27 15:21:04 +0500

    232. Раздел: Коллекция LinkedList. Решена задача "8. Вставка элемента в конец списка (метод offerLast)"

--- src/main/java/ru/begletsov/kodesource_tasks/_2_collections_list_linkedlist/_8_OfferLast.java
+++ src/main/java/ru/begletsov/kodesource_tasks/_2_collections_list_linkedlist/_8_OfferLast.java
@@ -3,16 +3,16 @@ package ru.begletsov.kodesource_tasks._2_collections_list_linkedlist;
 import java.util.LinkedList;
 
 /**
- * @Задание 7. Вставка элемента в начало списка (метод offerFirst)
+ * @Задание 8. Вставка элемента в конец списка (метод offerLast)
  * @Раздел Коллекция LinkedList
- * @Описание Напишите Java-программу для вставки указанного элемента в начало связанного списка.
+ * @Описание Напишите Java-программу для вставки указанного элемента в конец связанного списка.
  * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
  * @author Sergei Begletsov
  * @since 27.06.2021
  * @version 1
  */
 
-public class _7_OfferFirst {
+public class _8_OfferLast {
     public static void main(String[] args) {
         //create list and add some elements
         LinkedList<String> linkedList = new LinkedList<>();
@@ -21,9 +21,9 @@ public class _7_OfferFirst {
         linkedList.add("orange");
         //print list before
         System.out.println(linkedList);
-        //add elements at first position
-        linkedList.offerFirst("grey");
-        System.out.println("after add first element: " + linkedList.getFirst());
+        //add elements at last position
+        linkedList.offerLast("grey");
+        System.out.println("after add last element: " + linkedList.getLast());
         //print list after add
         System.out.println(linkedList);
     }
