6604ff99048464e9612933bd0f77f2bda890ed15
Sergei Begletsov <snower87@yandex.ru>
2021-07-11 14:06:00 +0500

    294. Раздел: HashMap. Задача "5. Пустая карта или нет?"

--- src/main/java/ru/begletsov/kodesource_tasks/_6_collections_map_hashmap/_5_IsEmptyMap.java
+++ src/main/java/ru/begletsov/kodesource_tasks/_6_collections_map_hashmap/_5_IsEmptyMap.java
@@ -3,7 +3,7 @@ package ru.begletsov.kodesource_tasks._6_collections_map_hashmap;
 import java.util.HashMap;
 
 /**
- * @Задание 4. Очистить карту
+ * @Задание 5. Пустая карта или нет?
  * @Раздел Коллекция HashMap
  * @Описание Напишите программу на Java, чтобы удалить все сопоставления с карты.
  * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
@@ -12,7 +12,7 @@ import java.util.HashMap;
  * @version 1
  */
 
-public class _4_ClearMap {
+public class _5_IsEmptyMap {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(1, "Red");
@@ -21,8 +21,10 @@ public class _4_ClearMap {
         hashMap.put(4, "Orange");
         hashMap.put(5, "Yellow");
         hashMap.put(6, "Blue");
-        System.out.println(hashMap);
+        System.out.println("hashMap: " + hashMap);
+        System.out.println("Size hashMap: " + hashMap.size() + ", isEmpty = " + hashMap.isEmpty());
+        System.out.println("AFTER remove all elements:");
         hashMap.clear();
-        System.out.println(hashMap);
+        System.out.println("Size hashMap: " + hashMap.size() + ", isEmpty = " + hashMap.isEmpty());
     }
 }
