1b36d5631c5489f13ec6cc70fa4eff17e9a0d2fa
Sergei Begletsov <snower87@yandex.ru>
2021-07-14 12:57:39 +0500

    306. Раздел: TreeMap. Задача "5. Получить набор ключей в TreeMap"

---
+++ src/main/java/ru/begletsov/kodesource_tasks/_7_collections_map_treemap/_5_KeySet.java
@@ -0,0 +1,29 @@
+package ru.begletsov.kodesource_tasks._7_collections_map_treemap;
+
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * @Задание 5. Получить набор ключей в TreeMap
+ * @Раздел Коллекция TreeMap
+ * @Описание Напишите программу на Java, чтобы получить все ключи из заданной древовидной карты.
+ * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
+ * @author Sergei Begletsov
+ * @since 14.07.2021
+ * @version 1
+ */
+
+public class _5_KeySet {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(1, "Red");
+        treeMap.put(2, "Green");
+        treeMap.put(3, "Black");
+        treeMap.put(4, "Orange");
+        treeMap.put(5, "Yellow");
+        treeMap.put(6, "Blue");
+        System.out.println("treeMap: " + treeMap);
+        Set<Integer> keySet = treeMap.keySet();
+        System.out.println(keySet);
+    }
+}
