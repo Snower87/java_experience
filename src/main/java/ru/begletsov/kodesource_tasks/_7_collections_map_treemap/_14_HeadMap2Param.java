5968e268eb0a451321b4e8ddd4e67d39beed674b
Sergei Begletsov <snower87@yandex.ru>
2021-07-18 14:26:14 +0500

    315. Раздел: TreeMap. Задача "14. Получить ключи, которые меньше или равны заданному ключу"

---
+++ src/main/java/ru/begletsov/kodesource_tasks/_7_collections_map_treemap/_14_HeadMap2Param.java
@@ -0,0 +1,32 @@
+package ru.begletsov.kodesource_tasks._7_collections_map_treemap;
+
+import java.util.TreeMap;
+
+/**
+ * @Задание 14. Получить ключи, которые меньше или равны заданному ключу
+ * @Раздел Коллекция TreeMap
+ * @Описание Напишите программу на Java, чтобы получить часть этой карты, ключи которой меньше (или равны, если inclusive true) данного ключа.
+ * ссылка на задание: http://kodesource.top/java-exercises/collection/index.php
+ * @author Sergei Begletsov
+ * @since 17.07.2021
+ * @version 1
+ */
+
+public class _14_HeadMap2Param {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(10, "Red");
+        treeMap.put(20, "Green");
+        treeMap.put(40, "Black");
+        treeMap.put(60, "Orange");
+        treeMap.put(70, "Yellow");
+        treeMap.put(80, "Blue");
+        System.out.println("treeMap: " + treeMap);
+        System.out.println("Checking the entry for 10: ");
+        System.out.println("Key(s): " + treeMap.headMap(10, true));
+        System.out.println("Checking the entry for 30: ");
+        System.out.println("Key(s): " + treeMap.headMap(30, true));
+        System.out.println("Checking the entry for 70: ");
+        System.out.println("Key(s): " + treeMap.headMap(70, true));
+    }
+}
