390a793da29ad8d736b8881fd1c60e5d252bf053
Sergei Begletsov <snower87@yandex.ru>
2021-08-02 17:17:53 +0500

    320. Решена задача "Remove String Spaces (8 kyu)". Codewars Kata

---
+++ src/main/java/ru/begletsov/codewars_task/_8_kyu/RemoveStringSpaces.java
@@ -0,0 +1,15 @@
+package ru.begletsov.codewars_task._8_kyu;
+
+/**
+ * @task Remove String Spaces (8 kyu). Codewars Kata
+ * Simple, remove the spaces from the string, then return the resultant string.
+ * @author Sergei Begletsov
+ * @since 02.08.2021
+ * @version 1
+ */
+
+public class RemoveStringSpaces {
+    public static String noSpace(final String x) {
+        return x.replace(" ","");
+    }
+}
