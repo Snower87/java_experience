ec949fc8f96af85cf27d8ef0d738ff880f561240
Sergei Begletsov <snower87@yandex.ru>
2021-10-03 07:52:04 +0500

    452. Заур Трегулов. JAVA – получи Чёрный Пояс. Глава: 4. Коллекции. Урок: 1. Введение в Collection и List

---
+++ src/main/java/ru/begletsov/video_courses/udemy/JAVA_get_black_belt/_4_collections/_1_intro/_1_IntroCollections.java
@@ -0,0 +1,23 @@
+package ru.begletsov.video_courses.udemy.JAVA_get_black_belt._4_collections._1_intro;
+
+/**
+ * = Папка 4. Коллекции =
+ * ---> Урок 1. Введение в Collection и List
+ * @author Sergei Begletsov
+ * @since 03.10.2021
+ * @version 1
+ */
+
+public class _1_IntroCollections {
+    public static void main(String[] args) {
+        //Иерархия коллекций:
+        //           Iterable
+        //              |
+        //          Collection
+        //     /        |           \
+        //1. List    2. Queue    3. Set
+
+        //List - упорядоченная последовательность элементов,
+        // позволяющая хранить дубликаты и null. Каждый элемент имеет индекс.
+    }
+}
