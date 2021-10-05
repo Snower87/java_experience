23390f574f4727003341fb5d30330858a888b9a2
Sergei Begletsov <snower87@yandex.ru>
2021-10-05 17:32:40 +0500

    461. Заур Трегулов. JAVA – получи Чёрный Пояс. Глава: 6. Lambda выражения. Урок: Урок 4. Supplier

---
+++ src/main/java/ru/begletsov/video_courses/udemy/JAVA_get_black_belt/_6_lambda/_4_supplier/Car.java
@@ -0,0 +1,22 @@
+package ru.begletsov.video_courses.udemy.JAVA_get_black_belt._6_lambda._4_supplier;
+
+public class Car {
+    String model;
+    String color;
+    double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
