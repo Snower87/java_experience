739b198928969dc0ab976d4af5183225db8e5761
Sergei Begletsov <snower87@yandex.ru>
2021-10-06 10:10:40 +0500

    468. Заур Трегулов. JAVA – получи Чёрный Пояс. Глава: 7. Streams. Урок: 5. Метод sorted

---
+++ src/main/java/ru/begletsov/video_courses/udemy/JAVA_get_black_belt/_7_stream/_5_sorted/_5_Sorted.java
@@ -0,0 +1,12 @@
+package ru.begletsov.video_courses.udemy.JAVA_get_black_belt._7_stream._5_sorted;
+
+/**
+ * = Папка 7. Streams =
+ * ---> Урок 5. Метод sorted
+ * @author Sergei Begletsov
+ * @since 06.10.2021
+ * @version 1
+ */
+
+public class _5_Sorted {
+}
