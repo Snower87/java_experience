d0fccece090f67e5190021dcafb4606a86f4dd3d
Sergei Begletsov <snower87@yandex.ru>
2021-10-06 15:52:22 +0500

    473. Класс File. Создание, переименование и удаление каталога

---
+++ src/main/java/ru/begletsov/basic_java/file/metanit/_3_CreateRemoveDeleteFile.java
@@ -0,0 +1,32 @@
+package ru.begletsov.basic_java.file.metanit;
+
+import java.io.File;
+
+/**
+ * Создание, переименование и удаление каталога
+ * @author Sergei Begletsov
+ * @since 06.10.2021
+ * @version 1
+ */
+
+public class _3_CreateRemoveDeleteFile {
+    public static void main(String[] args) {
+        //определяем объект для каталога
+        File dir = new File("F://SomeDir");
+        boolean created = dir.mkdir();
+        if (created) {
+            System.out.println("Folder has been created");
+        }
+        // переименуем каталог
+        File newDir = new File("F://NewSomeDir");
+        boolean renamed = dir.renameTo(newDir);
+        if (renamed) {
+            System.out.println("Folder has been renamed");
+        }
+        // удалим каталог
+        boolean deleted = newDir.delete();
+        if(deleted) {
+            System.out.println("Folder has been deleted");
+        }
+    }
+}
