2151022fa13ffbe56288ec18d9c95dadcc2566ce
Sergei Begletsov <snower87@yandex.ru>
2021-11-19 16:35:03 +0500

    555. Лафоре. Структуры данных и алгоритмы. Глава 4: Стеки и очереди. Раздел: Стеки

---
+++ src/main/java/ru/begletsov/from_books/lafore_structures_data_algorithms/chapter_3/_126_stack/StackApp.java
@@ -0,0 +1,23 @@
+package ru.begletsov.from_books.lafore_structures_data_algorithms.chapter_3._126_stack;
+
+public class StackApp {
+    public static void main(String[] args) {
+        StackX stack = new StackX(10);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.display();
+        stack.peek();
+        stack.pop();
+        stack.pop();
+        stack.display();
+        stack.peek();
+
+        while (!stack.isEmpty()) {
+            long popValue = stack.pop();
+            System.out.println("pop value: " + popValue);
+        }
+    }
+}
---
+++ src/main/java/ru/begletsov/from_books/lafore_structures_data_algorithms/chapter_3/_126_stack/StackX.java
@@ -0,0 +1,40 @@
+package ru.begletsov.from_books.lafore_structures_data_algorithms.chapter_3._126_stack;
+
+import java.util.Arrays;
+
+class StackX {
+    private int maxSize;
+    private long[] stackArray;
+    private int top;
+
+    public StackX(int size) {
+        maxSize = size;
+        stackArray = new long[maxSize];
+        top = -1;
+    }
+
+    public void push(long num) {
+        stackArray[++top] = num;
+    }
+
+    public long pop() {
+        return stackArray[top--];
+    }
+
+    public void peek() {
+        System.out.println("peek = " + stackArray[top]);
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public void display() {
+        System.out.println("Display array:");
+        Arrays.stream(stackArray).limit(top + 1).forEach(System.out::println);
+    }
+}
