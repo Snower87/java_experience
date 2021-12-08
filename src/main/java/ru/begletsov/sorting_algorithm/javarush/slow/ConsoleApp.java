2b719c1dc2f436a369caaa038d9e485a842e5bf6
Sergei Begletsov <snower87@yandex.ru>
2021-12-08 17:42:30 +0500

    578. Алгоритмы: Сортировка выбором (Selection Sort). JavaRush

--- src/main/java/ru/begletsov/sorting_algorithm/javarush/slow/ConsoleApp.java
+++ src/main/java/ru/begletsov/sorting_algorithm/javarush/slow/ConsoleApp.java
@@ -1,6 +1,7 @@
 package ru.begletsov.sorting_algorithm.javarush.slow;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ConsoleApp {
     public static void displayArray(int[] array) {
@@ -10,10 +11,31 @@ public class ConsoleApp {
     }
 
     public static void main(String[] args) {
+        //1. Входные данные
         //int[] array10 = {5, 4, 7, 9, 3, 8, 6, 2, 1};
         int[] array10 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] array100 = {37, 50, 23, 99, 0, 68, 11, 28, 33, 46, 55, 69, 73, 88, 40, 83, 9, 98, 15, 71};
-        displayArray(array10);
-        BubbleSort2 bubbleSort = new BubbleSort2(array10);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите способ сортировки:");
+        System.out.println("1. Сортировка пузырьком");
+        System.out.println("2. Сортировка выбором");
+        System.out.println("3. Сортировка вставками");
+        int num = scanner.nextInt();
+        switch (num) {
+            case 1: {
+                displayArray(array10);
+                BubbleSort2 bubbleSort = new BubbleSort2(array10);
+            } break;
+            case 2: {
+                displayArray(array10);
+                SelectionSort selectionSort = new SelectionSort(array10);
+            } break;
+            case 3: {
+
+            } break;
+            default:
+                break;
+        }
     }
 }
---
+++ src/main/java/ru/begletsov/sorting_algorithm/javarush/slow/SelectionSort.java
@@ -0,0 +1,47 @@
+package ru.begletsov.sorting_algorithm.javarush.slow;
+
+import java.util.Arrays;
+
+/**
+ * Class SelectionSort сортировка данных выбором
+ * неэффективная, сложность алгоритма O(n^2)
+ * @author Sergei Begletsov
+ * @since 08.12.2021
+ * @version 1
+ */
+
+public class SelectionSort {
+    private int step;
+
+    public void swap(int[] array, int ind1, int ind2) {
+        System.out.print("Переставляем элементы: ");
+        System.out.println(array[ind1] + ", " + array[ind2] +  " с индексами " + ind1 + ", " + ind2 + " соотв.");
+        int temp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = temp;
+    }
+
+    public void showArray(int[] array) {
+        Arrays.stream(array).forEach(a -> System.out.print(a + " "));
+        System.out.println();
+    }
+
+    public void showNumStep() {
+        System.out.println("Кол-во шагов: " + step);
+    }
+
+    SelectionSort(int[] array) {
+        for (int left = 0; left < array.length; left++) {
+            System.out.println("Индекс " + left);
+            int minInd = left;
+            for (int i = left; i < array.length; i++) {
+                if (array[i] < array[minInd]) {
+                    minInd = i;
+                    step++;
+                }
+            }
+            swap(array, left, minInd);
+        }
+        showNumStep();
+    }
+}
