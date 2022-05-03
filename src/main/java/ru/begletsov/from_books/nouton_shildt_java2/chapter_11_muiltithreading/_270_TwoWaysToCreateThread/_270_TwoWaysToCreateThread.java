00b08f97468009ed9b39fdd088be8e472fe44711
Sergei Begletsov <snower87@yandex.ru>
2022-05-03 07:34:45 +0500

    625. Ноутон, Шилдт. Java 2. Глава 11. Многопоточность. Раздел: 2 варианта создания потоков (270 стр.)

--- src/main/java/ru/begletsov/from_books/nouton_shildt_java2/chapter_11_muiltithreading/_270_TwoWaysToCreateThread/_270_TwoWaysToCreateThread.java
+++ src/main/java/ru/begletsov/from_books/nouton_shildt_java2/chapter_11_muiltithreading/_270_TwoWaysToCreateThread/_270_TwoWaysToCreateThread.java
@@ -5,6 +5,9 @@ public class _270_TwoWaysToCreateThread {
         //Вариант 1 - через реализацию интерфейса Runnable
         new ImplRunnable(); //создать новый поток
 
+        //Вариант 2 - через расширение класса Thread
+        new ExtThread();
+
         try {
             for (int i = 5; i > 0 ; i--) {
                 System.out.println("Таймер главн. потока: " + i);
@@ -17,13 +20,13 @@ public class _270_TwoWaysToCreateThread {
     }
 }
 
-class ImplRunnable implements Runnable{
+class ImplRunnable implements Runnable {
     Thread t;
 
     public ImplRunnable() {
         //Создать поток
         t = new Thread(this, "Demo Thread");
-        System.out.println("Дочерний поток: " + t);
+        System.out.println("Дочерний поток 1: " + t);
         //Запустить поток
         t.start();
     }
@@ -33,12 +36,35 @@ class ImplRunnable implements Runnable{
     public void run() {
         try {
             for (int i = 5; i > 0 ; i--) {
-                System.out.println("Таймер доч. потока: " + i);
+                System.out.println("Таймер доч. потока 1: " + i);
                 Thread.sleep(500);
             }
         } catch (InterruptedException e) {
-            System.out.println("Прерывание дочернего потока");
+            System.out.println("Прерывание дочернего потока 1");
         }
-        System.out.println("Завершение дочернего потока");
+        System.out.println("Завершение дочернего потока 1!");
+    }
+}
+
+class ExtThread extends Thread {
+
+    public ExtThread() {
+        super("Thread 2");
+        System.out.println("Дочерний поток 2: " + this);
+        //Запустить поток
+        start();
+    }
+
+    //точка входа для второго потока
+    public void run() {
+        try {
+            for (int i = 5; i > 0 ; i--) {
+                System.out.println("Таймер доч. потока 2: " + i);
+                Thread.sleep(700);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Прерывание доч. потока 2");
+        }
+        System.out.println("Завершение дочернего потока 2!");
     }
 }
