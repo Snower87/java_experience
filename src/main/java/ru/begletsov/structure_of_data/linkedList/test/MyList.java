21ad0d0a43d6bd47bf5154105fc32270b89ae5f0
Sergei Begletsov <snower87@yandex.ru>
2022-07-12 15:50:33 +0500

    626. Создание и разбор алгоритма работы односвязанного списка (http://axis.bplaced.net/news/773)

--- src/main/java/ru/begletsov/structure_of_data/linkedList/test/MyList.java
+++ src/main/java/ru/begletsov/structure_of_data/linkedList/test/MyList.java
@@ -4,42 +4,19 @@ package ru.begletsov.structure_of_data.linkedList.test;
  * @Задание Создание и разбор алгоритма работы односвязанного списка
  * @author Sergei Begletsov
  * @since 21.01.22
- * @version 1
+ * @version 2
  */
 
 public class MyList {
-    private ListElement head; //ссылка на первый элмент
-    private ListElement tail; //ссылка на последний элмент
+    private ListElement head; //ссылка на первый элемент
+    private ListElement tail; //ссылка на последний элемент
 
     public void addFirst(int data) {
-        ListElement element = new ListElement();
-        element.data = data;
-        if (head == null) {
-            head = element;
-            tail = element;
-        } else {
-            element.next = head;
-            head = element;
-        }
     }
 
     public void addLast(int data) {
-        ListElement element = new ListElement();
-        element.data = data;
-        if (tail == null) {
-            head = element;
-            tail = element;
-        } else {
-            tail.next = element;
-            tail = element;
-        }
     }
 
     public void printList() {
-        ListElement element = head;
-        while (element != null) {
-            System.out.println(element.data);
-            element = element.next;
-        }
     }
 }
