13fcd1097e21546ab8e999830f63edb507bc6803
Sergei Begletsov <snower87@yandex.ru>
2022-07-19 13:13:35 +0500

    634. Добавил методы для двусвязанного списка: insertFirst, insertLast

--- src/main/java/ru/begletsov/structure_of_data/doubleLinkedList/test/DLL.java
+++ src/main/java/ru/begletsov/structure_of_data/doubleLinkedList/test/DLL.java
@@ -16,10 +16,34 @@ public class DLL {
     //Вставка элемента в начало списка
     public void insertFirst(int data) {
         System.out.println("insertFirst -> " + data);
+        DLink vtx = new DLink(data);
+        vtx.next = head;
+        if (isEmpty()) {
+            tail = vtx;
+        } else {
+            head.previous = vtx;
+        }
+        head = vtx;
     }
 
     //Вставка элемента в конец списка
     public void insertLast(int data) {
         System.out.println("insertLast -> " + data);
+        DLink vtx = new DLink(data);
+        vtx.previous = tail;
+        if (tail == null) {
+            head = vtx;
+        } else {
+            tail.next = vtx;
+        }
+        tail = vtx;
+    }
+
+    public void printFirstToLast() {
+        DLink temp = head;
+        while (temp != null) {
+            System.out.println("{" + temp.data + "}");
+            temp = temp.next;
+        }
     }
 }
--- src/main/java/ru/begletsov/structure_of_data/doubleLinkedList/test/DLLApplication.java
+++ src/main/java/ru/begletsov/structure_of_data/doubleLinkedList/test/DLLApplication.java
@@ -3,6 +3,34 @@ package ru.begletsov.structure_of_data.doubleLinkedList.test;
 public class DLLApplication {
     public static void main(String[] args) {
         DLL doubleLinkedList = new DLL();
+        System.out.println("Пример работы с двусвязанным списком: ");
+        int data = 50;
+        System.out.println("Вставка элемента " + data + " в начало списка");
+        doubleLinkedList.insertFirst(data);
+        data = 40;
+        System.out.println("Вставка элемента " + data + " в начало списка");
+        doubleLinkedList.insertFirst(data);
+        data = 30;
+        System.out.println("Вставка элемента " + data + " в начало списка");
+        doubleLinkedList.insertFirst(data);
 
+        System.out.println();
+        System.out.println("Содержимое всего списка:");
+        doubleLinkedList.printFirstToLast();
+
+        data = 10;
+        System.out.println("Вставка элемента " + data + " в конец списка");
+        doubleLinkedList.insertLast(data);
+        data = 11;
+        System.out.println("Вставка элемента " + data + " в конец списка");
+        doubleLinkedList.insertLast(data);
+        data = 12;
+        System.out.println("Вставка элемента " + data + " в конец списка");
+        doubleLinkedList.insertLast(data);
+
+
+        System.out.println();
+        System.out.println("Содержимое всего списка:");
+        doubleLinkedList.printFirstToLast();
     }
 }
