16769a8a0d101998f59819ff41b15b4e015f535b
Sergei Begletsov <snower87@yandex.ru>
2022-07-21 17:33:46 +0500

    639. Лафоре. Структуры данных и алгоритмы. Глава 8. Двоичные деревья. Заготовка

---
+++ src/main/java/ru/begletsov/from_books/lafore_structures_data_algorithms/chapter_8/_355_tree/Node.java
@@ -0,0 +1,7 @@
+package ru.begletsov.from_books.lafore_structures_data_algorithms.chapter_8._355_tree;
+
+public class Node {
+    int data;
+    Node leftChild;
+    Node rightChild;
+}
---
+++ src/main/java/ru/begletsov/from_books/lafore_structures_data_algorithms/chapter_8/_355_tree/Tree.java
@@ -0,0 +1,27 @@
+package ru.begletsov.from_books.lafore_structures_data_algorithms.chapter_8._355_tree;
+
+public class Tree {
+    private Node root;
+
+    /**
+     * Поиск узла по ключу
+     * @param key значение ключа
+     */
+    public void find(int key) {
+    }
+
+    /**
+     * Вставка новых данных
+     * @param id данные
+     */
+    public void insert(int id) {
+    }
+
+    /**
+     * Удаление узла по ключу
+     * @param id данные
+     */
+    public void delete(int id) {
+
+    }
+}
