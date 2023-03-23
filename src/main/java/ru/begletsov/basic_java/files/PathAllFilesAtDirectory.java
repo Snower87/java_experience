ad3c42613d550b114f7083d55805fe98d89fca2b
Sergei Begletsov <snower87@yandex.ru>
2023-03-23 19:14:25 +0500

    645. Добавил методы сортировки и вывода на консоль списка файлов

--- src/main/java/ru/begletsov/basic_java/files/PathAllFilesAtDirectory.java
+++ src/main/java/ru/begletsov/basic_java/files/PathAllFilesAtDirectory.java
@@ -2,18 +2,36 @@ package ru.begletsov.basic_java.files;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Класс PathAllFilesAtDirectory Дерево папок с названиями файлов (с разделителем). Для дисков H, G
+ * 1) базовый функционал 2) добавил методы: сортировки и вывода на консоль
  */
 
 public class PathAllFilesAtDirectory {
     static ArrayList list = new ArrayList();
+    static List<String> strings = new ArrayList<>();
+
+    public static void sortList() {
+        Collections.sort(strings);
+    }
+
+    public static void showAllAtList() {
+        for (String str: strings) {
+            System.out.println(str);
+        }
+    }
 
     public static void main(String[] args) {
-        File dir = new File("G:\\Games");
+        File dir = new File("G:\\");
         //File dir = new File("H:\\");
         showFiles(dir.listFiles());
+        sortList();
+        System.out.println("=== SORTED LIST ===");
+        showAllAtList();
+        System.out.println("end");
     }
 
     public static void showFiles(File[] files) {
@@ -37,9 +55,10 @@ public class PathAllFilesAtDirectory {
             }
         }
 
-        if (str.contains("G:\\Games")) {
-            if (numDelim == 4) {
-                System.out.println(str.substring(9));
+        if (str.contains("G:\\")) {
+            if (numDelim == 3) {
+                System.out.println(str.substring(3));
+                strings.add(str.substring(3));
             }
         }
 
